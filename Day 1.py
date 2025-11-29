21bc4105f975cf11259413b0d01bc0625334bd7d
sumitk7782 <sumitk7782@gmail.com>
2025-11-29 14:57:56 +0530

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,37 @@
+
+
+#PROJECT
+num1= float(input("Enter a 1st number: "))
+num2=float(input("Enter a 2nd number: "))
+print(f"1st number is",{num1} , "And 2nd number is ", {num2})
+print("Addtion: ",num1+num2)
+print("Substract: ",num1-num2)
+print("Multiple: ",num1*num2)
+print("divide: ",num1/num2)
+
+
+c=float(input("Enter temperature in celcius:"))
+f = (c * 9/5)+32
+print("Temperature in fahrenheit:",f)
+
+
+f=float(input("Enter temperature in fahrenhei:"))
+f = (f- 32)*5/9
+print("Temperature in celcius:",f)
+
+mm= float(input("Enter a value millimetre:"))
+meter= mm/100
+print("Metre:",meter)
+
+cm = float(input("Enter a centimeter:"))
+m = cm/100
+print("Meter",m)
+
+m = float(input("Enter a meter:"))
+cm=m*100
+print("Centimeter", cm)
+
+
+
+
+
